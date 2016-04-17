02f50863e7801fb9ca61c8d19dbd96c7bcc99819
Emil Maric <emil.maric@hotmail.com>
2016-04-17 15:17:53 -0700

    Added brew scripts

---
+++ brew/brew_scripts/backup_homebrew.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+brew_command=/usr/local/bin/brew
+brew_cask_command="$brew_command cask"
+
+echo '#!/bin/bash'
+echo ''
+echo 'brew_command=/usr/local/bin/brew'
+echo 'brew_cask_command="$brew_command cask"'
+echo ''
+echo 'failed_items=""'
+echo ''
+echo 'function install_package() {'
+echo '    echo EXECUTING: brew install $1 $2'
+echo '    $brew_command install $1 $2'
+echo '    [ $? -ne 0 ] && $failed_items="$failed_items $1"  # package failed to install.'
+echo '}'
+echo ''
+echo 'function install_cask_package() {'
+echo '    echo EXECUTING: brew cask install $1'
+echo '    $brew_cask_command install $1'
+echo '    [ $? -ne 0 ] && $failed_items="$failed_items $1"  # package failed to install.'
+echo '}'
+echo ''
+
+echo '# Taps'
+$brew_command tap | while read tap; do echo "$brew_command tap $tap"; done
+echo ''
+
+echo '# Formulas'
+$brew_command list | while read item;
+do
+    echo "install_package $item '$($brew_command info $item | /usr/bin/grep 'Built from source with:' | /usr/bin/sed 's/^[ \t]*Built from source with:/ /g; s/\,/ /g')'"
+done
+echo ''
+
+echo '# Casks'
+$brew_cask_command list | while read item;
+do
+    echo "install_cask_package $item"
+done
+echo ''
+
+echo '[ ! -z $failed_items ] && echo The following items were failed to install: && echo $failed_items'
