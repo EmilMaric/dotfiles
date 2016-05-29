42affea753486fa52fb1a5c73bf1b30b78bc82e1
Emil Maric <emil.maric@hotmail.com>
2016-05-29 11:46:04 -0700

    Updated bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,6 @@
 
 # Install Oh My Zsh framework for zsh plugins
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Change shell to zsh
+chsh -s /usr/local/bin/zsh
